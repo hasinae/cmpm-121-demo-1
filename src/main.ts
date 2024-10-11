13989cf2e3fe3af95cf9638c19867d682de85aec
hasina <113416278+hasinae@users.noreply.github.com>
2024-10-11 10:24:19 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My great game";
+const gameName = "Cherry Clicker!";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+
+ // Creating a button that the player can click
+const button = document.createElement("button");
+button.innerHTML = "Click 🍒! ";
+app.append(button);
