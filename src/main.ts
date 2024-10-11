495e3b388721f025c528dc0dafe7c141c3914848
hasina <113416278+hasinae@users.noreply.github.com>
2024-10-11 10:35:49 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,19 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-
- // Creating a button that the player can click
+// Step 1: A button you can click
 const button = document.createElement("button");
 button.innerHTML = "Click 🍒! ";
 app.append(button);
+
+// Step 2: Clicking increases a counter
+let counter: number = 0;
+
+const counterDisplay = document.createElement("div");
+counterDisplay.innerHTML = `${counter} cherry points 🍒`;
+app.append(counterDisplay);
+
+button.addEventListener("click", () => {
+    counter++;
+    counterDisplay.innerHTML = `${counter} cherry points 🍒`;
+});
